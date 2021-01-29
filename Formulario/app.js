cbcba66fc9ca891a6acde12c164719c83af6b84d
Emanuel Cueva <joseemanuelc@gmail.com>
2021-01-28 18:05:47 -0600

    commit

--- Formulario/app.js
+++ Formulario/app.js
@@ -1,5 +1,7 @@
 var nameuser = 'ecueva07'
 var passuser = 'Mane07'
+var matriculaa = '0052'
+var nombalumno = "Jessica Nayeli Contreras Zamora"
 
 function buscarusua() {
     var usuariotemp = document.getElementById('usuario')
@@ -17,11 +19,28 @@ function buscarusua() {
     }
 }
 
+function buscaralumno() {
+    var matriculatemp = document.getElementById('matricula')
+
+    if (matriculatemp.value === matriculaa) {
+        document.getElementById('NombreAlumno').innerHTML = nombalumno;
+    } else {
+        document.getElementById('NombreAlumno').innerHTML = 'Alumno no encontrado';
+        window.alert("Matricula Incorrecta")
+
+    }
+}
+
 function pulsarenter(e) {
     tecla = (document.all) ? e.keyCode : e.which;
     if (tecla == 13) buscarusua(); {
 
     }
+}
 
+function pulsarenter1(e) {
+    tecla = (document.getElementById('matricula')) ? e.keyCode : e.which;
+    if (tecla == 13) buscaralumno(); {
 
+    }
 }
